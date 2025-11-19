e2d9f3768e86ab6f23aee8be45ace78038f58796
Divya-pingali <divi.pingali@gmail.com>
2025-11-19 18:13:12 +0800

    Tabs changes

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,54 @@
 import { Tabs } from "expo-router";
+import { Icon } from "react-native-paper";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="post" />
-      <Tabs.Screen name="profile" />
-      <Tabs.Screen name="signOut" />
+    <Tabs 
+      screenOptions={{ 
+        tabBarPosition: "bottom",
+        tabBarShowLabel: true,
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#858585",
+        tabBarActiveBackgroundColor: "#F8F5FF",
+        tabBarInactiveBackgroundColor: "#ffffff",
+        tabBarItemStyle: {
+          borderRadius: 10
+        },
+      }}
+    >
+      <Tabs.Screen 
+        name="home" 
+        options = {{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Icon source="home" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen 
+        name="post" 
+        options={{
+          title: "Post",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Icon source="plus-box" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen 
+        name="profile" 
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Icon source="account" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen 
+        name="signOut"
+        options = {{
+          title: "Sign Out",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Icon source="logout" color={color} size={size} />,
+        }}
+      />
     </Tabs>
   );
 }
